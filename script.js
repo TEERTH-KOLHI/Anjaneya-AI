91fe8a846fe77fa640837dcbd53ac1889c58e010
apexwrk-code <apexwrk@gmail.com>
2026-03-16 11:06:06 +0500

    updates done

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('active');
+            } else {
+                entry.target.classList.remove('active');
             }
         });
     };
